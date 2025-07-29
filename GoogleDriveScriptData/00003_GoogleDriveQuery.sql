22d7aa6d2c9e2c0176d7b4a18d45b7f81a7da7ce
GiaHuy2505 <phangiahuy25052003@gmail.com>
2025-07-29 13:26:36 +0700

    update

--- GoogleDriveScriptData/00003_GoogleDriveQuery.sql
+++ GoogleDriveScriptData/00003_GoogleDriveQuery.sql
@@ -14,6 +14,7 @@ GO
 -- select User Setting
 Declare @userId int = 1;
 select 
+	su.SettingUserId,
 	a.UserName as UserName,
 	s.SettingKey,
 	s.SettingValue
@@ -26,7 +27,8 @@ where a.UserId =@userId
 -- select login user file
 Declare @LoginUser int = 1 
 select 
-	a.UserName ,
+	uf.FileId,
+	a.UserName,
 	uf.UserFileName
 from UserFile uf 
 join Account a on uf.OwnerId = a.UserId
@@ -36,7 +38,8 @@ where a.UserId =@LoginUser
 -- select login user folder
 Declare @userId int = 1
 select 
-	a.UserName ,
+	fo.FolderId,
+	a.UserName,
 	fo.FolderName
 from Folder fo
 join Account a on fo.OwnerId = a.UserId
@@ -45,7 +48,8 @@ where a.UserId =@userId
 
 -- select shared file with login user
 Declare @userId int = 102
-select 
+select
+	f.FileId,
 	a.UserName,
 	f.UserFileName
 from SharedUser su
@@ -61,7 +65,8 @@ select * from Shareduser where shareId =2
 
 -- select shared folder with login user
 Declare @userId int = 101
-select 
+select
+	fo.FolderId,
 	a.UserName,
 	fo.FolderName
 from SharedUser su
@@ -74,9 +79,10 @@ select * from share where ObjectTypeId =1
 select * from SharedUser where ShareId = 1
 
 
---Recomment file/folder
+--Recomment file
 Declare @userId int = 2
 select top 10
+	f.FileId,
 	a.UserName,
 	f.UserFileName,
 	ar.ActionLog,
@@ -93,6 +99,7 @@ select * from ActionRecent
 --Recomment folder
 Declare @userId int = 3
 select top 10
+	fo.FolderId,
 	a.UserName,
 	fo.FolderName,
 	ar.ActionLog,
@@ -109,7 +116,8 @@ select * from ActionRecent
 -- trash screen
 --select file have been deleted
 Declare @userId int = 704
- SELECT 
+ SELECT
+	f.FileId,
     t.TrashId,
     ot.ObjectTypeName,
     f.UserFileName,
@@ -126,6 +134,7 @@ select * from Trash
 --select folder have been deleted
 Declare @userId int = 1
  SELECT 
+	fo.FolderId,
     t.TrashId,
     ot.ObjectTypeName,
     fo.FolderName,
@@ -141,6 +150,7 @@ WHERE t.UserId = @userId;
 -- Select file
 Declare @userId int = 794
 select 
+	f.FileId,
 	f.UserFileName,
 	a.USerName as FileOwnerName,
 	a.UserId as UserId,
@@ -158,6 +168,7 @@ select * from UserFile f where f.FileId = 875
 --product Screen
 -- select all of product
 select
+	ProductId,
 	ProductName,
 	Cost,
 	Duration
@@ -167,6 +178,7 @@ from ProductItem
 -- select Product bought by user
 Declare @userId int = 100
 select 
+	pro.ProductId,
 	pro.ProductName,
 	a.UserId,
 	a.UserName,
@@ -187,6 +199,7 @@ select * from UserProduct up where up.UserId = 100
 
 -- select Top 10 Payers 
 select top 10
+	a.UserId,
 	pro.ProductName,
 	a.UserName,
 	case
@@ -200,39 +213,56 @@ join Promotion po on up.PromotionId = po.PromotionId
 order by TotalCost DESC
 
 
--- select file/folder shared for user with userid = 101
+-- select folder shared for user with userid = 101
 Declare @userId int = 101
 select 
 	a.UserName,
+	fo.FolderId,
 	p.PermissionName,
-	f.UserFileName,
 	fo.FolderName
 from SharedUser su
 join Account a on su.SharedUserId = a.UserId
 join Share s on su.ShareId = s.ShareId
 join Permission p on su.PermissionId = p.PermissionId
 LEFT join Folder fo on s.ObjectTypeId = 1 and fo.FolderId = s.ObjectId
+where su.UserId = @userId
+
+-- select file shared for user with userid = 101
+Declare @userId int = 102
+select 
+	a.UserName,
+	p.PermissionName,
+	f.FileId,
+	f.UserFileName
+from SharedUser su
+join Account a on su.SharedUserId = a.UserId
+join Share s on su.ShareId = s.ShareId
+join Permission p on su.PermissionId = p.PermissionId
 LEFT join UserFile f on s.ObjectTypeId = 2 and f.FileId = s.ObjectId
 where su.UserId = @userId
 
 
-select * from SharedUser su where su.SharedUserId = 500
+select * from SharedUser su where su.ShareId = 2
 select * from share s where s.ShareId = 1
 select * from UserFile f where f.FileId = 298
 select * from Account where UserId = 101
 
 
---select top 5 largest file 
+--select top 5 largest file of login-user 
+DECLARE @userId int = 4
 select distinct top 5
 	f.UserFileName,
 	a.UserName as OwnerName,
 	f.Size as FileSize
 from UserFile f
 join Account a on f.OwnerId = a.UserId
+where f.OwnerId = @userid
 order by f.Size DESC
 
+select * from UserFile where OwnerId =4
 
---select banned user
+--select banned user of login-user
+DECLARE @userId int = 533
 SELECT 
     BU.Id AS BanId,
     Banned.UserName AS BannedUserName,
@@ -241,12 +271,14 @@ SELECT
 FROM BannedUser BU
 JOIN Account Banned ON BU.UserId = Banned.UserId
 JOIN Account Banner ON BU.BannedUserId = Banner.UserId
+where BU.UserId = @userId
 ORDER BY BU.BannedAt DESC;
 
 
 --select product bought by user 
 Declare @userId int = 100
 select 
+	p.ProductId,
 	p.ProductName,
 	a.UserName,
 	up.PayingDatetime,
@@ -291,19 +323,17 @@ from Share
 where ObjectId = 654
 
 
--- select user was shared object with objectId = 5
+-- select user was shared object with objectId = 5 and objectType is folder
 Declare @objectId int = 5
 select 
 	a.UserName,
-	f.UserFileName,
 	fo.FolderName,
 	p.PermissionName
 from Share s
 	join SharedUser su on s.ShareId = su.ShareId
 	join Account a on su.UserId = a.UserId
 	join Permission p on su.PermissionId = p.PermissionId
-	left join UserFile f on s.ObjectTypeId =2 and s.ObjectId = f.FileId
-	left join [Folder] fo on s.ObjectTypeId =1 and s.ObjectId = fo.FolderId
+	left join Folder fo on s.ObjectTypeId =1 and s.ObjectId = fo.FolderId
 where s.ObjectId = @objectId
 
 
@@ -396,6 +426,7 @@ where FileId = 1
 declare @Sharer int = 2
 declare @shared int = 102
 select 
+	uf.FileId,
 	ft.Icon,
 	uf.UserFileName as NameOfFile,
 	a1.UserName as SharerName,
@@ -416,6 +447,7 @@ declare @OwnerId int = 1
 declare @FileType int = 3
 
 Select 
+	uf.FileId,
 	ft.Icon,
 	uf.UserFileName,
 	a.UserName as OwnerName,
