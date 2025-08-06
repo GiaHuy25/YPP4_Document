a120de0ee940a0c4621ff82fd4a97e375cced656
GiaHuy2505 <phangiahuy25052003@gmail.com>
2025-08-06 10:26:23 +0700

    update file presentation and mslists query

--- MsLists/MsListsQuery.sql
+++ MsLists/MsListsQuery.sql
@@ -26,3 +26,58 @@ select
 from ListTemplate lt
 join TemplateProvider tp on lt.ProviderId = tp.Id
 order by lt.Title
+
+
+--- Detail of template ---
+declare @templateId int = 1
+select 
+	lt.Icon,
+	lt.Title,
+	lt.HeaderImage,
+	lt.Sumary,
+	lt.Feature
+from ListTemplate lt
+where lt.Id = @templateId
+
+
+
+--- sample data for template ---
+declare  @listTemplateId int = 5
+select 
+	tcl.ColumnName,
+	tr.DisplayOrder,
+	tc.CellValue
+from TemplateColumn tcl
+cross join TemplateSampleRow tr
+left join TemplateSampleCell tc on tc.TemplateColumnId = tcl.Id and tc.TemplateSampleRowId = tr.Id
+where tcl.ListTemplateId = @listTemplateId
+
+--- Get data of list ---
+SELECT *
+FROM (
+    SELECT 
+        ld.ColumnName,
+        lr.DisplayOrder,
+        lv.CellValue
+    FROM ListDynamicColumn ld
+    CROSS JOIN ListRow lr
+    LEFT JOIN ListCellValue lv 
+        ON lv.ListColumnId = ld.Id 
+       AND lv.ListRowId = lr.Id
+    WHERE ld.ListId = 5 
+      AND lr.ListId = 5
+) AS SourceTable
+PIVOT (
+    MAX(CellValue) 
+    FOR ColumnName IN (
+        Created_By,
+		ID,
+		col_2_TEXT,
+		Attachment,
+		Title,
+		Modified_By,
+		col_1_INT,
+		col_3_NUMBER
+    )
+) AS PivotTable
+order by DisplayOrder
